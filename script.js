36c614643b412963c25628f43ba7f8c1d2f96b13
Talip <talipakcelik@hacettepe.edu.tr>
2022-01-16 22:05:26 +0300

    storeBoard is filling according to players' mark index on the screen

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 "use strict";
 
-/// DOM
-
-// const grid1 = document.getElementById("1").getAttribute("id");
-// const grid2 = document.getElementById("2").getAttribute("id");
-// const grid3 = document.getElementById("3").getAttribute("id");
-// const grid4 = document.getElementById("4").getAttribute("id");
-// const grid5 = document.getElementById("5").getAttribute("id");
-// const grid6 = document.getElementById("6").getAttribute("id");
-// const grid7 = document.getElementById("7").getAttribute("id");
-// const grid8 = document.getElementById("8").getAttribute("id");
-// const grid9 = document.getElementById("9").getAttribute("id");
-
+// DOM
 const grid1 = document.querySelector(".item-1");
 const grid2 = document.querySelector(".item-2");
 const grid3 = document.querySelector(".item-3");
@@ -22,31 +11,6 @@ const grid7 = document.querySelector(".item-7");
 const grid8 = document.querySelector(".item-8");
 const grid9 = document.querySelector(".item-9");
 const grid = document.querySelectorAll(".grid");
-
-// const gameBoard = {
-//   gameBoard: ["x", "o", "x", "o", "x", "o"],
-// };
-
-// game state data //
-
-const player1 = {};
-const player2 = {};
-
-const gameFlow = {};
-
-// const playerCreator = function (mark, active = false) {
-//   const players = {};
-//   players.mark = mark;
-//   players.active = active;
-//   players.toggle = function () {
-//     if (this.active === true) {
-//       this.active = false;
-//     } else {
-//       this.active === true;
-//     }
-//   };
-//   return players;
-// };
 
 const playerCreator = function (name, mark, active) {
   let state = {};
@@ -69,140 +33,51 @@ const playerCreator = function (name, mark, active) {
 };
 
 const Tttgame = (function () {
-  const gameBoard = ["X", "0", "X", "0", "X", "0", "X", "0", "X"];
+  const gameBoard = ["", "", "", "", "", "", "", "", ""];
 
   let players = [
     playerCreator("Gandalf", "X", true),
     playerCreator("Sauron", "O", false),
   ];
 
-  let activePlayer = players[0].name;
+  let activePlayer = players[0];
 
   const switchPlayer = function () {
-    if (activePlayer === players[0].name) activePlayer = players[1].name;
-    else activePlayer = players[0].name;
+    if (activePlayer === players[0]) activePlayer = players[1];
+    else activePlayer = players[0];
     console.log(activePlayer);
   };
 
-  const displayController = function {
-    board.addEventListener("click", function() {
-      e.target.textContent = activePlayer.mark // burayı activePlayer.mark player yapabilmek için players[0]'dan sonra name kalktı. 
-    })
-  }
-
+  const gameFlow = function () {};
   const draw = function () {
     let n = 0;
-    Gameboard.gameBoard.forEach(() => {
-      document.getElementById(`${n}`).textContent = Gameboard.gameBoard[n];
+    gameBoard.forEach(() => {
+      document.getElementById(`${n}`).textContent = gameBoard[n];
       n++;
     });
   };
-  // console.log(players[1]);
-  // const player1 = playerCreator("X", true);
-  // const player2 = playerCreator("O", false);
 
-  // function resetBoard() {
-  //   gameBoard.gameboard = ["", "", "", "", "", "", "", "", ""];
-  // }
+  let storeBoard = [];
 
-  return { players, switchPlayer, activePlayer };
-})();
-
-let activePlayer = "X";
-
-// const items = [
-//   [`${grid1}`, `${grid2}`, `${grid3}`],
-//   [`${grid4}`, `${grid5}`, `${grid6}`],
-//   [`${grid7}`, `${grid8}`, `${grid9}`],
-// ];
-const items = [
-  [``, ``, ``],
-  [``, ``, ``],
-  [``, ``, ``],
-];
-
-// document.querySelector("div").textContent = `${gameBoard.board}`;
-
-const board = document.querySelector(".board");
-
-const loopGameBoard = function (e) {
-  for (const x of Gameboard.gameBoard) {
-    e.target.textContent = x;
-  }
-};
-
-const draw = function () {
-  let n = 0;
-  Gameboard.gameBoard.forEach(() => {
-    document.getElementById(`${n}`).textContent = Gameboard.gameBoard[n];
-    n++;
-  });
-};
-
-// function draw() {
-//   let n = 0;
-//   boardState.forEach(() => {
-//     document.getElementById(`game-cell-${n + 1}`).textContent = boardState[n];
-//     n++;
-//   });
-// }
-const items2 = [];
-
-// board.addEventListener("click", function (e) {
-//   if (e.target.textContent !== "X" && e.target.textContent !== "O") {
-//     e.target.textContent = Gameboard.gameBoard[0];
-//   }
-
-//   items2[`${e.target.getAttribute("id")}`] = e.target.textContent;
-// });
-
-// board.addEventListener("click", function (e) {
-//   e.target.textContent = `${gameBoard.board[0]}`;
-// });
-
-// factory function
-
-function userCreator(name, score) {
-  const newUser = {};
-  newUser.name = name;
-  newUser.score = score;
-  newUser.increment = function () {
-    newUser.score++;
-  };
-  return newUser;
-}
-
-const user1 = userCreator("Will", 2);
-const user2 = userCreator("Tim", 5);
-user1.increment();
-
-// The Module Pattern
-const ShoppingCart2 = (function () {
-  const cart = [];
-  const shippingCost = 10;
-  const totalPrice = 237;
-  const totalQuantity = 23;
-
-  const addToCart = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(
-      `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
-    );
-  };
-
-  const orderStock = function (product, quantity) {
-    console.log(`${quantity} ${product} ordered from supplier`);
+  const displayController = function () {
+    board.addEventListener("click", function (e) {
+      const index = e.target.getAttribute("id");
+      console.log(index);
+      if (e.target.textContent !== "X" && e.target.textContent !== "O") {
+        console.log("dede");
+        storeBoard[index] = activePlayer.mark;
+        e.target.textContent = activePlayer.mark;
+        switchPlayer();
+      }
+    });
   };
 
   return {
-    addToCart,
-    cart,
-    totalPrice,
-    totalQuantity,
+    storeBoard,
+    players,
+    switchPlayer,
+    activePlayer,
+    displayController,
+    draw,
   };
 })();
-
-ShoppingCart2.addToCart("apple", 4);
-ShoppingCart2.addToCart("pizza", 2);
-console.log(ShoppingCart2);
-console.log(ShoppingCart2.shippingCost);
