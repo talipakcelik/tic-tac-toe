325c8026a08cf755c0115cd29531b23211b02661
Talip <talipakcelik@hacettepe.edu.tr>
2022-01-18 16:00:40 +0300

    .every() method inside .find() method to checkWin

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 "use strict";
 
-// DOM
-// const grid1 = document.querySelector(".item-1");
-// const grid2 = document.querySelector(".item-2");
-// const grid3 = document.querySelector(".item-3");
-// const grid4 = document.querySelector(".item-4");
-// const grid5 = document.querySelector(".item-5");
-// const grid6 = document.querySelector(".item-6");
-// const grid7 = document.querySelector(".item-7");
-// const grid8 = document.querySelector(".item-8");
-// const grid9 = document.querySelector(".item-9");
-// const grid = document.querySelectorAll(".grid");
-
 const playerCreator = function (name, mark, active) {
   let state = {};
   (state.name = name), (state.mark = mark), (state.active = active);
@@ -68,55 +56,49 @@ const Tttgame = (function () {
       storeBoard[index] = activePlayer.mark;
       e.target.textContent = activePlayer.mark;
       checkDraw();
-      checkWin3();
+      if (checkWin(activePlayer.mark) !== false) {
+        console.log(`${activePlayer.name} is won`);
+        board.removeEventListener("click", displayController);
+      }
       switchPlayer();
     }
   };
+
   const startGame = function () {
     board.addEventListener("click", displayController);
   };
 
-  const checkWin2 = function (indexes, mark) {
-    let checkStoreBoard = [
-      storeBoard[indexes[0]],
-      storeBoard[indexes[1]],
-      storeBoard[indexes[2]],
-    ];
-
-    let result = checkStoreBoard.every((index) => index === mark);
-    let draw = storeBoard.every((index) => index === "X" || index === "O");
-    if (result === true) {
-      console.log(`${activePlayer.name} is won`);
-      board.removeEventListener("click", displayController);
-    } else if (draw === true) console.log("draw");
-  };
-
   const checkDraw = function () {
     let draw = storeBoard.every((index) => index === "X" || index === "O");
-    if (draw === true && checkWin2 === false) console.log("draw");
+    if (draw === true && checkWin(activePlayer.mark) === false)
+      console.log("draw");
   };
 
-  const checkWin3 = function () {
-    checkWin2([0, 1, 2], activePlayer.mark);
-    checkWin2([3, 4, 5], activePlayer.mark);
-    checkWin2([6, 7, 8], activePlayer.mark);
-    checkWin2([2, 4, 6], activePlayer.mark);
-    checkWin2([0, 4, 8], activePlayer.mark);
-    checkWin2([0, 3, 6], activePlayer.mark);
-    checkWin2([1, 4, 7], activePlayer.mark);
-    checkWin2([2, 5, 8], activePlayer.mark);
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [2, 4, 6],
+    [0, 4, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+
+  const checkWin = function (mark) {
+    return (
+      winConditions.find((winCondition) =>
+        winCondition.every((element) => storeBoard[element] === mark)
+      ) || false
+    );
   };
 
   return {
-    displayController,
     refresh,
-    storeBoard,
-    checkWin2,
-    checkWin3,
     startGame,
   };
 })();
 
 function init() {
-  Tttgame.displayController();
+  Tttgame.startGame();
 }
